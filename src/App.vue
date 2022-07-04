d5cf530f5176e8bd6db403549428bd3c3dd34562
musicpop <playmusic8@gmail.com>
2022-07-04 18:13:37 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">模版--基于vue、webpack构建umd组件 {{ testData }}</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    testData: {
+      type: String,
+      default: '',
+    },
+  },
+  mixins: [],
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style scoped src="./assets/css/app.scss" lang="scss"></style>
